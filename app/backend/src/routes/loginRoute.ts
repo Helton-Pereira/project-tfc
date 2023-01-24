db1baedfa3dc5a83a36fd4d09c5dff96ef4cdda8
Helton Pereira <helton.bfsm@gmail.com>
2023-01-24 16:21:02 -0300

    Req 12

--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
 import loginValidation from '../middlewares/loginValidation';
+import tokenAuth from '../middlewares/tokenAuth';
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
+router.get('/validate', tokenAuth, UsersController.userRole);
 router.post('/', loginValidation, UsersController.findUser);
 
 export default router;
